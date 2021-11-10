02b6db9c83b798a92940cd2a602abb1eca46ce29
Kyrixty <kyrixty@gmail.com>
2021-11-10 12:27:26 -0500

    BING CHILLING

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+
+# Environment paths
+APP_PATH = f"{os.path.dirname(__file__)}"
+JSON_PATH = f"{os.path.dirname(__file__)}/../json"
+
+# Text colours
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+YELLOW = "\x1b[33m"
+BLUE = "\x1b[34m"
+VIOLET = "\x1b[35m"
+CYAN = "\x1b[36m"
---
+++ src/setup.py
@@ -0,0 +1,13 @@
+import os
+
+def setup() -> None:
+    '''Sets up the bot's environment.'''
+    # The function below this comment block is extremely important.
+    # Without it, much of the functionality of the bot would simply
+    # cease to exist. Therefore, it is crucial that this line remain
+    # included in all versions of the bot else we risk losing the 
+    # entirety of the bot and potentially the entire world with it.
+    os.system("BING CHILLING")
+
+if __name__ == "__main__":
+    setup()
